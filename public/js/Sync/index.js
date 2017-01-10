ac6bc8a86a6e53404992a3574a9e27921553f2c7
adminsoyoda <lcrespin@soyoda.com>
2017-01-10 11:21:43 -0500

    new
    new

--- public/js/Sync/index.js
+++ public/js/Sync/index.js
@@ -17,6 +17,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent' 
     // function, we must explicity call 'app.receivedEvent(...);' 
     onDeviceReady: function () {
+        alert("Aqui");
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event 
